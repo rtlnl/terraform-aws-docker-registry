f61370091332f3ca69a6aa1cbc0fff84ca56cf72
Niels ten Boom <nielstenboom@gmail.com>
2020-07-14 10:38:51 +0200

    Update main.tf

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -5,8 +5,8 @@ resource "aws_s3_bucket" "docker_registry_bucket" {
 }
 
 module "docker-registry" {
-  source = "github.com/rtlnl/terraform-aws-docker-registry"
-  version = "1.0.0"
+  source  = "rtlnl/docker-registry/aws"
+  version = "0.0.1"
 
   http_secret            = "randomlygeneratedstring"
   s3_access_key          = "<s3_access_key>"
